afc557b1893d0d8a756541c74dc693338cce9e9c
unknown <shraddha-prakash.bhat@tu-ilmenau.de>
2025-03-02 22:39:27 +0100

    Add .env to .gitignore and update code

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/local", {
+    await mongoose.connect("mongodb+srv://SHraddhabhat:<db_password>@cluster0.3erwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
