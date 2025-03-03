59a6547d4a90ad855e1234b62e00bf310fe5a68b
Shraddha Prakash Bhat <83150988+Shraddha151@users.noreply.github.com>
2025-03-03 12:05:11 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
-    await mongoose.connect("mongodb+srv://shraddhabhat151@gmail.com:<Spbhat151@>@cluster0.3erwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-=======
-    await mongoose.connect("mongodb+srv://shraddhab955@gmail.com:<Spbhat151@>@cluster0.3erwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
->>>>>>> ddde41f (Merge remote changes with my local updates)
+    await mongoose.connect("mongodb+srv://SHraddhabhat:<Spbhat151@>@cluster0.3erwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
